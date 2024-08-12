bcda5a6dd39323fac4709fbe3990e53abd06802c
chanyoung.choi <chanyoung.choi@innowireless.com>
2024-08-12 15:36:55 +0900

    init

---
+++ auth/src/main/java/com/example/auth/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.example.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to auth service.";
+    }
+}
